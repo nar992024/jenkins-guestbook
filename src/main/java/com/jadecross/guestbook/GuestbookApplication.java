ebc1ae24171419a0464bfe1a569d9ac067bbe24b
nar992024 <nar99@naver.com>
2024-08-02 20:21:41 +0900

    Update GuestbookApplication.java

--- src/main/java/com/jadecross/guestbook/GuestbookApplication.java
+++ src/main/java/com/jadecross/guestbook/GuestbookApplication.java
@@ -2,10 +2,36 @@ package com.jadecross.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Counter;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RestController
 public class GuestbookApplication {
 
+    private final MeterRegistry meterRegistry;
+    private Counter counter;
+
+    public GuestbookApplication(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.counter = this.meterRegistry.counter("requests_total");
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        counter.increment();
+        return "Hello " + name;
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(GuestbookApplication.class, args);
 	}
